828bd521740420a014f72a7f436a8f587cd08cfc
Jake Samuels <samuels242@googlemail.com>
2024-12-29 14:47:34 +0000

    property pulse

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import "@/assets/styles/globals.css";
 
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 export const metadata = {
   title: "Propery Pulse",
   keywords: "rental, property, real estate",
@@ -10,7 +13,11 @@ const MainLayout = ({ children }) => {
   return (
     <html>
       <body>
-        <main>{children}</main>
+        <main>
+          <Navbar />
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
